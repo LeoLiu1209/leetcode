99b406722c1019d29c85e999f179704d7ebccc94
Leo Liu <cubemail88@gmail.com>
2019-05-11 15:59:27 +0800

    Create 104.MaximumDepthofBinaryTree.java

---
+++ topinter/104.MaximumDepthofBinaryTree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/*
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+*/
+
+/*
+    hint : 二元樹的特性就是每個節點最多只會有兩個子節點，所以只要分別算左右節點的總數，取大的就行了。
+*/
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null)
+            return 0;
+        return 1 + Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+}
