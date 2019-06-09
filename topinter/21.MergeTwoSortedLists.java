1c572ce55b8358e10b09ff7c014fb8a11875508a
Leo Liu <cubemail88@gmail.com>
2019-06-09 16:14:29 +0800

    add 21.rec version

--- topinter/21.MergeTwoSortedLists.java
+++ topinter/21.MergeTwoSortedLists.java
@@ -36,4 +36,21 @@ class Solution {
             tmp.next = l1;
         return head.next;
     }
+    
+     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+         ListNode tmp = null;
+         if(l1 == null)
+            return l2;
+         if(l2 == null)
+             return l1;
+         if(l1.val < l2.val){
+             tmp  = l1;
+             tmp.next = mergeTwoLists(l1.next, l2);
+         }
+         else {
+             tmp = l2;
+             tmp.next = mergeTwoLists(l1, l2.next);
+         }
+         return tmp;
+     }
 }
